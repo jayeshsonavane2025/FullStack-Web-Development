15c98839b9cba70425afade7498f60a50c92d91e
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-01 18:47:52 +0530

    password generator  updated

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -15,10 +15,45 @@ const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 //initially values
 let password = "";
-let passwordLength = 10;
+let passwordLength = 20;
 let checkCount = 0;
 handleSlider();
 
 
 //ste strength circle color to grey
 setIndicator("#ccc");
+//set passwordLength
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+     
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%"
+}
+
+function setIndicator(color) {
+    indicator.style.backgroundColor = color;
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomNumber() {
+    return getRndInteger(0,9);
+}
+
+function generateLowerCase() {  
+       return String.fromCharCode(getRndInteger(97,123))
+}
+
+function generateUpperCase() {  
+    return String.fromCharCode(getRndInteger(65,91))
+}
+
+function generateSymbol() {
+    const randNum = getRndInteger(0, symbols.length);
+    return symbols.charAt(randNum);
+}
