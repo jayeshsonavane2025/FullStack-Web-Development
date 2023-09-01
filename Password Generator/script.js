79b1323f44ac89e754d39617d9a9fe3d37d0dfbd
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-01 19:42:09 +0530

    calcStrength

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -11,10 +11,10 @@ const symbolsCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateButton");
 const allCheckBox = document.querySelectorAll("input[type=checkbox]");
-const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+const symbols = '~`{[}]|:;"<,!@#$%^&*()_-+=>.?/';
 
 //initially values
-let password = "";
+let password = ""; 
 let passwordLength = 20;
 let checkCount = 0;
 handleSlider();
@@ -56,4 +56,28 @@ function generateUpperCase() {
 function generateSymbol() {
     const randNum = getRndInteger(0, symbols.length);
     return symbols.charAt(randNum);
+}
+ 
+ 
+function calcStrength() {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
 }
